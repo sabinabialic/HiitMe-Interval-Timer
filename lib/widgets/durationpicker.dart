0719ed607e00d036defc241501ff3d8b2e02199c
Sabina <sabina.bialic@gmail.com>
2021-01-19 20:34:53 -0500

    Duration Picker
    Added dependency for the number picker, began working on the logic for the duration picker widget

--- lib/widgets/durationpicker.dart
+++ lib/widgets/durationpicker.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/cupertino.dart';
 
+// Widget obtained from https://pub.dev/packages/numberpicker
+import 'package:numberpicker/numberpicker.dart';
+
 class DurationPicker extends StatefulWidget {
+  final Duration initDuration;
+  final Widget confirmWidget;
+  final Widget cancelWidget;
+
+  DurationPicker({
+    @required this.initDuration,
+    Widget confirmWidget,
+    Widget cancelWidget
+  }) : confirmWidget = confirmWidget ?? new Text('Ok'),
+        cancelWidget = cancelWidget ?? new Text('Cancel');
+
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
+  State<StatefulWidget> createState() => new _DurationPickerState(initDuration);
+}
+
+class _DurationPickerState extends State<DurationPicker> {
+  int min;
+  int sec;
+
+  // Constructor
+  _DurationPickerState(Duration defaultDuration) {
+    min = defaultDuration.inMinutes;
+    sec = defaultDuration.inSeconds % Duration.secondsPerMinute;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     throw UnimplementedError();
   }
 }
