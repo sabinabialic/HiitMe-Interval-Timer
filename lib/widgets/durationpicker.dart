4f2c1f5ea87696e1d4f2cb6c05a113782e8af3a8
Sabina <sabina.bialic@gmail.com>
2021-01-22 11:22:02 -0500

    Duration Picker
    Implementing duration picker logic

--- lib/widgets/durationpicker.dart
+++ lib/widgets/durationpicker.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 // Widget obtained from https://pub.dev/packages/numberpicker
 import 'package:numberpicker/numberpicker.dart';
 
 class DurationPicker extends StatefulWidget {
   final Duration initDuration;
+  final Widget title;
   final Widget confirmWidget;
   final Widget cancelWidget;
 
   DurationPicker({
     @required this.initDuration,
+    this.title,
     Widget confirmWidget,
     Widget cancelWidget
   }) : confirmWidget = confirmWidget ?? new Text('Ok'),
@@ -31,7 +34,48 @@ class _DurationPickerState extends State<DurationPicker> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return new AlertDialog(
+      title: widget.title,
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Number picker for minutes
+          new NumberPicker.integer(
+              initialValue: min,
+              minValue: 0,
+              maxValue: 10,
+              // When the value changes, the state needs to be updated
+              onChanged: (value) { this.setState(() { min = value; }); }
+          ),
+
+          // Colon separator between minutes and seconds
+          Text (":", style: TextStyle(fontSize: 22)),
+
+          // Number picker for seconds
+          new NumberPicker.integer(
+              initialValue: sec,
+              minValue: 0,
+              maxValue: 59,
+              // When the value changes, the state needs to be updated
+              onChanged: (value) { this.setState(() { sec = value; }); }
+          ),
+        ],
+      ),
+
+      actions: [
+        // If the user clicks cancel, go back to the main page
+        new FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: widget.cancelWidget),
+        // If the user clicks ok, the new values need to be updated
+        new FlatButton(
+            onPressed: () => Navigator.of(context).pop(
+                new Duration(
+                    minutes: min,
+                    seconds: sec)
+            ),
+            child: widget.confirmWidget)
+      ],
+    );
   }
 }
